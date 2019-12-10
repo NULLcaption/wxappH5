378fcf0d48f69ac1f43cee656cae8c514155a646
NULLcaption <cxg1207@126.com>
2019-12-10 08:27:27 +0800

    Update Constant.java

--- src/main/java/com/cxg/weChat/core/config/Constant.java
+++ src/main/java/com/cxg/weChat/core/config/Constant.java
@@ -7,15 +7,15 @@ package com.cxg.weChat.core.config;
 **/
 public class Constant {
     //meco公众号的appId和appSercet
-    public static final String M_APP_ID = "wxe9bec4afeb0d2985";
-    public static final String M_APP_SERCET = "be42b75c0ab7256af0cdef6192c16def";
+    public static final String M_APP_ID = "";
+    public static final String M_APP_SERCET = "";
     //xpp公众号的appId和appSercet
-    public static final String X_APP_ID = "wx98e9f0f06ca7d370";
-    public static final String X_APP_SERCET = "e904388a7894fcc7c77bd042f62ca201";
+    public static final String X_APP_ID = "";
+    public static final String X_APP_SERCET = "";
     //LFY公众号的appId
-    public static String L_APP_APPID = "wx8d30f85a90b200f3";
+    public static String L_APP_APPID = "";
     //请求地址
-    public static String URL = "http://ssl.zjxpp.com";
+    public static String URL = "";
     public static String TEST_URL = "http://10.3.25.11";
 
     //获取access_token的url
